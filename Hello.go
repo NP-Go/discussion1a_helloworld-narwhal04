0505499e567df98b64fa0a4030b14321e7188e44
narwhal04 <q.quek7@gmail.com>
2022-03-28 12:15:13 +0800

    added var and code function

--- Hello.go
+++ Hello.go
@@ -1,12 +1,15 @@
 package main
 
+import "fmt"
+
 func HelloWorld() string {
-	//Insert your code here
+	messageHelloWorld := "Hello World from Go!"
 
 	//Do not remove this line here
 	return messageHelloWorld
 }
 
 func main() {
-	HelloWorld()
+
+	fmt.Println(HelloWorld())
 }
